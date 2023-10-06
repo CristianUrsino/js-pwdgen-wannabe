a474aa142f6663c9d3fc30c1efd281ed212e238d
CristianUrsino <ursino.cristian.uc@gmail.com>
2023-10-06 15:23:37 +0200

    optimized algorithm

--- script/script.js
+++ script/script.js
@@ -1,40 +1,15 @@
-//MODO 1
-/*let nome = prompt('inserire nome: ');
-let cognome = prompt('inserisci cognome: ');
-let colore = prompt('inserire colore preferito: ');
-const fixedNumber = 21;
-let saluto = `
+
+const nome = prompt('inserire nome: ');
+const cognome = prompt('inserisci cognome: ');
+const colore = prompt('inserire colore preferito: ');
+const fixedNumber = "21";
+const saluto = `
     <h2>
-        `+ nome + cognome + colore + fixedNumber.toString() +`
-    </h2>
-`;
-
-document.getElementById('testo').innerHTML = saluto;*/
-
-//MODO 2
-/*let nome = prompt('inserire nome: ');
-let cognome = prompt('inserisci cognome: ');
-let colore = prompt('inserire colore preferito: ');
-const fixedNumber = 21;
-const password = nome + cognome + colore + fixedNumber;
-let saluto = `
-    <h2>
-        `+ password +`
-    </h2>
-`;
-
-document.getElementById('testo').innerHTML = saluto;*/
-
-//MODO 3 
-let password = prompt('inserire nome: ');
-password += prompt('inserisci cognome: ');
-password += prompt('inserisci colore: ');
-password += '21';
-
-let saluto = `
-    <h2>
-        `+ password +`
+        ${nome + cognome + colore + fixedNumber}
     </h2>
 `;
 
 document.getElementById('testo').innerHTML = saluto;
+
+
+
