4c593e6656206e20fcb711d5bdceb24a5fcb4c04
CristianUrsino <ursino.cristian.uc@gmail.com>
2023-10-06 16:48:00 +0200

    added style and copy algorithm

--- script/script.js
+++ script/script.js
@@ -3,13 +3,18 @@ const nome = prompt('inserire nome: ');
 const cognome = prompt('inserisci cognome: ');
 const colore = prompt('inserire colore preferito: ');
 const fixedNumber = "21";
-const saluto = `
-    <h2>
-        ${nome + cognome + colore + fixedNumber}
-    </h2>
-`;
+const saluto = `${nome + cognome + colore + fixedNumber}`;
 
 document.getElementById('testo').innerHTML = saluto;
 
-
+function copia(_testo) {
+    var input = document.createElement('input');
+    var area = document.getElementById(_testo).value;
+    input.setAttribute('value', area);
+    document.body.appendChild(input);
+    input.select();
+    var risultato = document.execCommand('copy');
+    document.body.removeChild(input);
+    return risultato;
+ }
 
